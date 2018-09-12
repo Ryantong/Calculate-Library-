79eb48d03435ea9f4e88dcdbd6af31abb51e5fc4
Ryantong <20tongr31@mymhs.org>
2018-09-12 11:04:04 -0700

    Version 0 square and cube

---
+++ Access Calculate/src/DoMath.java
@@ -0,0 +1,11 @@
+/*This is the client code for the Calculate Library
+ * call various methods in the Calculate Library 
+ * @author Ryan Tong
+ * @version 0 September 6, 2018
+ */
+public class DoMath {
+	public static void main(String[] args) {
+		System.out.println(Calculate.square(-3));
+		System.out.println(Calculate.cube(-2));
+	}
+}
---
+++ Calculate Library/src/Calculate.java
@@ -0,0 +1,14 @@
+/* This class contains the methods to do various math related tasks. 
+ * @author Ryan Tong 
+ * @version 0 September 6, 2018
+ */
+public class Calculate {
+	//returns the square of an integer 
+	public static int square(int number) {
+		return number * number;
+	}
+	//returns the cube of an integer 
+	public static int cube(int number) {
+		return number * number * number;
+	}
+}
