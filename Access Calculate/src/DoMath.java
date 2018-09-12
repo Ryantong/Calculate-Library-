de73fc0695a6f5cd081ee1b19b9952259c2e86e6
Ryantong <20tongr31@mymhs.org>
2018-09-12 11:30:17 -0700

    square, cube average(2,3inputs), toDegrees, and toRadians

--- Access Calculate/src/DoMath.java
+++ Access Calculate/src/DoMath.java
@@ -7,5 +7,10 @@ public class DoMath {
 	public static void main(String[] args) {
 		System.out.println(Calculate.square(-3));
 		System.out.println(Calculate.cube(-2));
+		System.out.println(Calculate.average(3.0, 9.0));
+		System.out.println(Calculate.average(15.0, 17.0, 18.0));
+		System.out.println(Calculate.toDegrees(2));
+		System.out.println(Calculate.toRadians(1));
+		System.out.println(Calculate.toDegrees(Calculate.toRadians(2)));//randominversetest
 	}
 }
--- Calculate Library/src/Calculate.java
+++ Calculate Library/src/Calculate.java
@@ -11,4 +11,27 @@ public class Calculate {
 	public static int cube(int number) {
 		return number * number * number;
 	}
+	//returns the average of two doubles
+	public static double average(double num1, double num2) {
+		return (num1 + num2)/2;
+	}
+	//overloaded method, returns average of three doubles
+	public static double average(double num1, double num2, double num3) {
+		return (num1 + num2 + num3)/3;
+	}
+	//returns degrees(double) of the inputed radians(double)
+	public static double toDegrees(double radians) {
+		return radians * 180 / 3.14159;
+	}
+	//returns radians(double) of the inputed degrees(double)
+	public static double toRadians(double degrees) {
+		return degrees * 3.14159 / 180;
+	}
 }
+
+
+
+
+
+
+
